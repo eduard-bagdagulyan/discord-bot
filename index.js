d68f80497b6867305b909a78651d55f97cbb9d6c
Eduard Bagdagulyan <eduard.bagdagulyan2003@gmail.com>
2021-09-19 22:56:10 +0400

    .env fix

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 const discord = require('discord.js');
-
+require('dotenv').config()
+console.log(process.env.DISCORD_TOKEN_BOT);
 const client = new discord.Client({ disableMentions: 'everyone' });
 
 const { Player } = require('discord-player');
@@ -36,4 +37,4 @@ for (const file of player) {
     client.player.on(file.split(".")[0], event.bind(null, client));
 };
 
-client.login(client.config.discord.token);
+client.login(process.env.DISCORD_TOKEN_BOT);
