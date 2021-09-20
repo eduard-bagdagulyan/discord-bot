2f0b9fb74e88f3c9e31f2ffc4af8bebfe94045c7
Eduard Bagdagulyan <eduard.bagdagulyan2003@gmail.com>
2021-09-20 22:35:19 +0400

    actvity fix

--- player/channelEmpty.js
+++ player/channelEmpty.js
@@ -1,3 +1,4 @@
 module.exports = (client, message, queue) => {
     message.channel.send(`${client.emotes.error} - Music stopped as there is no more member in the voice channel !`);
+    client.user.setActivity('SHTAB Exoneri Hamar', {type: 'PLAYING'});
 };
--- player/queueEnd.js
+++ player/queueEnd.js
@@ -1,3 +1,4 @@
 module.exports = (client, message, queue) => {
     message.channel.send(`${client.emotes.error} - Music stopped as there is no more music in the queue !`);
+    client.user.setActivity('SHTAB Exoneri Hamar', {type: 'PLAYING'});
 };
