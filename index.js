3632d718d67a20dabc211b57582d761e37841d0c
Eduard Bagdagulyan <eduard.bagdagulyan2003@gmail.com>
2021-09-20 22:06:15 +0400

    trying to realtime update activity

--- index.js
+++ index.js
@@ -11,8 +11,6 @@ client.config = require('./config/bot');
 client.emotes = client.config.emojis;
 client.filters = client.config.filters;
 client.commands = new discord.Collection();
-// client.user.setActivity()
-client.setActivity('Test', {type: 'STREAMING'})
 
 fs.readdirSync('./commands').forEach(dirs => {
     const commands = fs.readdirSync(`./commands/${dirs}`).filter(files => files.endsWith('.js'));
