dc624a9fee5703b8ad916d8c1e1f884b438a6b18
Karapetyan Ashot <ashot.karapetyan.y@gmail.com>
2021-10-08 23:08:56 +0400

    fixing seek

--- commands/music/seek.js
+++ commands/music/seek.js
@@ -9,14 +9,14 @@ module.exports = {
     async execute(client, message, args) {
         const queue = player.getQueue(message.guild.id);
 
-        if (!queue || !queue.playing) return message.channel.send(`No music currently playing ${message.author}... try again ? ❌`);
+        if (!queue || !queue.playing) return await message.channel.send(`No music currently playing ${message.author}... try again ? ❌`);
 
         const timeToMS = ms(args.join(' '));
 
-        if (timeToMS >= queue.current.durationMS) return message.channel.send(`The indicated time is higher than the total time of the current song ${message.author}... try again ? ❌\n*Try for example a valid time like **5s, 10s, 20 seconds, 1m**...*`);
+        if (timeToMS >= queue.current.durationMS) return await message.channel.send(`The indicated time is higher than the total time of the current song ${message.author}... try again ? ❌\n*Try for example a valid time like **5s, 10s, 20 seconds, 1m**...*`);
 
         await queue.seek(timeToMS);
 
-        message.channel.send(`Time set on the current song **${ms(timeToMS, { long: true })}** ✅`);
+        await message.channel.send(`Time set on the current song **${ms(timeToMS, { long: true })}** ✅`);
     },
 };
