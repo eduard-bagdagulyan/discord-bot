467444058057f27130f2eb8933b8faff9c6717cf
Karapetyan Ashot <ashot.karapetyan.y@gmail.com>
2021-10-08 23:24:21 +0400

    fixing seek

--- commands/music/seek.js
+++ commands/music/seek.js
@@ -1,6 +1,19 @@
-const { player } = require('discord-player');
+const { Player } = require('discord-player');
 const { Client, Intents } = require('discord.js');
 
+global.client = new Client({
+    intents: [
+        Intents.FLAGS.GUILDS,
+        Intents.FLAGS.GUILD_MEMBERS,
+        Intents.FLAGS.GUILD_MESSAGES,
+        Intents.FLAGS.GUILD_VOICE_STATES
+    ],
+    disableMentions: 'everyone',
+});
+
+client.config = require('./config');
+
+global.player = new Player(client, client.config.opt.discordPlayer);
 
 
 module.exports = {
